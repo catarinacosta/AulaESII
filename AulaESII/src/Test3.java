815d3cb4b435aee503a71435f495616d21da8f63
dayrocha <dady.rmb@gmail.com>
2015-12-09 21:36:28 -0500

    Vem férias!

--- AulaESII/src/Test3.java
+++ AulaESII/src/Test3.java
@@ -10,6 +10,17 @@
  */
 public class Test3 {
 	
-	System.out.println("To change this...")
-	
+	public static void main(String[] args) {
+		int soma = 0;
+		int i = 1; 
+		
+		while (i <= 1000) {
+			soma = soma + i;
+			i++;
+		}
+		
+		System.out.println("A soma deu " + soma);
+
+	}
+
 }
